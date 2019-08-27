5056ce35c6f9c9c2172b0609d602b36be5f5e9a8
Camille De Peretti <camilledeperetti10@gmail.com>
2019-08-27 17:28:00 +0200

    first step foursquare

--- app/controllers/nights_controller.rb
+++ app/controllers/nights_controller.rb
@@ -6,7 +6,13 @@ class NightsController < ApplicationController
         lat: midpoint[0],
         lng: midpoint[1]
         # infoWindow: render_to_string(partial: "info_window", locals: { night: night})
+        # image_url: helpers.asset_url('REPLACE_THIS_WITH_YOUR_IMAGE_IN_ASSETS')
       }
+
+    # send the midpoint to foursquare
+    # create radius around midpoint
+    #retrieve 3 bars in the raidus
+    #iterate over the 3 bars to display info
     end
   end
 end
