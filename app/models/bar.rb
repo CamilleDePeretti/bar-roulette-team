51f4681e79bcab7cfb140473659379fc99d65b98
Camille De Peretti <camilledeperetti10@gmail.com>
2019-08-29 19:16:13 +0200

    fixing model

--- app/models/bar.rb
+++ app/models/bar.rb
@@ -1,6 +1,7 @@
 class Bar < ApplicationRecord
   belongs_to :night
+  validates :name, presence: true, allow_blank: false
+  validates :address, presence: true, allow_blank: false
 end
 
-validates :name, presence: true, allow_blank: false
-validates :address, presence: true, allow_blank: false
+
