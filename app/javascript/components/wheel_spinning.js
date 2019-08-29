d12afe01ac226baf99d118112713fb3003d3d494
Camille De Peretti <camilledeperetti10@gmail.com>
2019-08-29 18:31:29 +0200

    gif randomization

--- app/javascript/components/wheel_spinning.js
+++ app/javascript/components/wheel_spinning.js
@@ -1,13 +1,3 @@
-// When I click on the wheel button
-// <iframe src="https://giphy.com/embed/Nd2hAfzpUhzJVrAnPN" width="480" height="360" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/julesm-julesmumm-psycheledic-Nd2hAfzpUhzJVrAnPN">via GIPHY</a></p>
-
-// the home page disappear
-
-//for 5 sec
-
-//redircets to show page
-
-
 const initGiphy = () => {
   const wheelBtn = document.getElementById("address-form-submit");
   console.log(wheelBtn);
