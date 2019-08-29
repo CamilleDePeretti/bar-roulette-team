78fc8cefcd0283d2c927ca7e8dd98b09d3e48242
Victor Menge <vfmenge@gmail.com>
2019-08-29 16:08:03 +0200

    Refactored the bar creation code into a service class, made an exception catcher for exceeding the photo API quota, and made the bar search look for a minimum of x bars and increase the radius of the searcy by z when it doesn't find it, recursively looking for more bars

--- app/controllers/nights_controller.rb
+++ app/controllers/nights_controller.rb
@@ -17,13 +17,13 @@ class NightsController < ApplicationController
     @night.lat = coords[0]
     @night.lng = coords[1]
     @night.save
-    create_bars(@night)
+    BarCreationService.create_bars(@night)
     redirect_to night_path(@night)
   end
 
   def show
     @night = Night.find(params[:id])
-    @markers = @night.bars.map do |bar|
+    @markers = @night.bars[0..2].map do |bar|
       @bar_name = bar.name
       @bar_address = bar.address
     {
@@ -39,24 +39,5 @@ class NightsController < ApplicationController
       infoWindow: render_to_string(partial: "info_window"),
       image_url: helpers.asset_url('midpoint-logo.png')
     }
-  end
-
-  private
-
-  def create_bars(night)
-    client = Foursquare2::Client.new(:client_id => 'TRIBS0HCJFDS3USY5MQ2Z2GSIOTK5H1E312LKTQBZIKAMYTT', :client_secret => ENV['FOURSQUARE_SECRET_KEY'])
-    @results = client.search_venues(:ll => "#{@night.lat}, #{@night.lng}", :radius => '250', :limit => '3', :categoryId => '4bf58dd8d48988d116941735,50327c8591d4c4b30a586d5d,4bf58dd8d48988d121941735', :v => '20190827')
-    @results.to_hash['venues'].each do |result|
-      name = result['name'].to_s
-      lat = result['location']['lat'].to_s
-      lng = result['location']['lng'].to_s
-      address = result['location']['formattedAddress'].join(', ')
-      category = result['categories'].first['name'].to_s
-      foursquare_id = result['id'].to_s
-      Bar.create(name: name, lat: lat, lng: lng, address: address, category: category, night: night, foursquare_id: foursquare_id)
-      photo_response = client.venue_photos(foursquare_id, :v => '20190827').to_hash
-      photo_hash = photo_response['items'].first
-      photo_url = photo_hash['prefix'] + '512x512' + photo_hash['suffix']
-    end
   end
 end
