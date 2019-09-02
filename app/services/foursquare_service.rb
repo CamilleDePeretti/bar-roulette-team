a4999d4c2093a84f6a69b1898b27dcd783ad741b
Victor Menge <vfmenge@gmail.com>
2019-09-02 12:09:44 +0200

    Created a separate FourSquareServie class

---
+++ app/services/foursquare_service.rb
@@ -0,0 +1,55 @@
+class FoursquareService
+  def self.bars_get(night, amnt, radius, increment)
+    puts "Looking for #{amnt} bars in a #{(radius/1_000.0).round(2)}km radius"
+    results = client.search_venues(:ll => "#{night.lat}, #{night.lng}", :radius => radius, :limit => amnt, :categoryId => '4bf58dd8d48988d116941735,50327c8591d4c4b30a586d5d,4bf58dd8d48988d121941735', :v => '20190827').to_hash
+    print_results(results)
+
+    return results if radius > 100_000
+    multiplier = 1.2
+    multiplier = 1.5 if radius >= 1_500
+    multiplier = 3 if radius >= 20_000
+
+    if results['venues'].length < amnt
+      sleep 0.5 # Maximum 2 QPS, so sleeping just to make sure
+      return bars_get(night, amnt, radius + increment, increment * multiplier)
+    end
+
+    puts "#{results['venues'].length}"
+    results
+  end
+
+  def self.photo_get(foursquare_id)
+    begin
+      photo_response = client.venue_photos(foursquare_id, :v => '20190827').to_hash
+    rescue Foursquare2::APIError
+      puts "API Quota exceeded... (Photo request)"
+      return ""
+    end
+
+    if photo_response['items'].any?
+      photo_hash = photo_response['items'].first
+      return photo_hash['prefix'] + '512x512' + photo_hash['suffix']
+    end
+
+    ""
+  end
+
+  def self.hours_get(foursquare_id)
+    begin
+      hours_response = client.venue_hours(foursquare_id, :v => '20190827').to_hash
+    rescue Foursquare2::APIError
+      puts "API Quota exceeded... (Hours request)"
+      return ""
+    end
+
+    return hours_response['hours']['timeframes'][0]['open'][0] if hours_response['hours'].any?
+
+    ""
+  end
+
+  private
+
+  def self.client
+    Foursquare2::Client.new(:client_id => ENV['FOURSQUARE_PUBLIC_KEY'], :client_secret => ENV['FOURSQUARE_SECRET_KEY'])
+  end
+end
